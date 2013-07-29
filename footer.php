81622793e0940dc893b167aeb26119b4811e7a0d
Học Nguyễn <nguyenhoc@xiao.vn>
2013-07-29 18:41:08 +0700

    Create footer.php
    code footer.php in login_004.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+</div>
+
+  <div id="footer">
+      <p class="copy fl">Copyright 2013<strong> <a href="http://xiao.vn" style="
+    color: white;
+">Xiao JSC</a> </strong>. All rights reserved.</p>
+      <ul class="button language_button white fr">
+        <li class="icon_single language fl"></li>
+        <li class="flag vn fl"></li>
+		<li class="flag en fl"></li>
+		<li class="flag es fl"></li>
+        <li class="flag de fl"></li>
+        <li class="flag it fl"></li>
+        <li class="clear"></li>
+      </ul>
+	  
+	  <ul class="skinner fr">
+		<li class="fl"><a href="#" rel="style_blue" class="styleswitch skin skin_blue fl"></a></li>
+		<li class="fl"><a href="#" rel="style_green" class="styleswitch skin skin_green fl"></a></li>
+		<li class="fl"><a href="#" rel="style_red" class="styleswitch skin skin_red fl"></a></li>
+		<li class="fl"><a href="#" rel="style_purple" class="styleswitch skin skin_purple fl"></a></li>
+		<li class="clear"></li>
+	 </ul>
+    </div>
+</body>
+</html>
